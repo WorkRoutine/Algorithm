593a546de2cda49280d1c23976c736db35f7e5ac
BuddyZhang1 <buddy.d.zhang@gmail.com>
2017-09-25 09:49:49 +0800

    add GPL

--- base/linked_list/base.c
+++ base/linked_list/base.c
@@ -1,5 +1,11 @@
 /*
- * list demo
+ * Single list base demo
+ *
+ * (C) 2017.09 <buddy.zhang@aliyun.com>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation.
  */
 #include <stdio.h>
 #include <stdlib.h>
